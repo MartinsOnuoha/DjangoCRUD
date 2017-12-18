29a3649ddbd403e155905187cab245ffc0e6b8d2
MartinsOnuoha <martinsvictor.nd@gmail.com>
2017-12-18 22:57:45 +0100

    tests for class methods

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,72 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question, Choice
 
 # Create your tests here.
+class QuestionMethodTests(TestCase):
+
+    
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published recently method should return False for questions
+        whose pub_date is in the future
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
+        self.assertEqual(timezone.now().day, datetime.datetime.now().day)
+    
+    
+    def test_string_representation_of_object_is_returned(self):
+        """
+        question_text should be returned when Question object is called
+        """
+        question = Question()
+        self.assertEqual(question.__str__(), question.question_text)
+    
+    
+    def test_was_published_recently_with_recent_question(self):
+            """
+            was_published_recently should return true for whose
+            pub_date is within the last days
+            """
+            time = timezone.now() - datetime.timedelta(hours=1)
+            recent_question = Question(pub_date=time)
+            self.assertEqual(recent_question.was_published_recently(), True)
+            
+    def test_Question_attributes_type(self):
+            """
+            check that methof variables hold proper value
+            and value types
+            """
+            question = Question()
+            boolean = question.was_published_recently.boolean
+            admin_field = question.was_published_recently.admin_order_field
+            short_description = question.was_published_recently.short_description
+            self.assertEqual(type(admin_field), str)
+            self.assertEqual(admin_field, 'pub_date')
+            self.assertEqual(type(boolean), bool)
+            self.assertEqual(boolean, not None)
+            self.assertEqual(type(short_description), str)
+            
+        
+                
+            
+# Choice Class
+class ChoiceAttributeTests(TestCase):
+    
+    def test_choice_text_value_type(self):
+        is_a_string = str
+        choice = Choice()
+        self.assertEqual(type(choice.choice_text), is_a_string)
+        
+    def test_vote_value_type(self):
+        is_an_int = int
+        choice = Choice()
+        choice.question = Question(question_text='how are you', pub_date=timezone.now())
+        self.assertEqual(type(choice.votes), is_an_int)
+        
+        
--- polls/views.py
+++ polls/views.py
@@ -1,32 +1,56 @@
 # /polls/views.py
 
-# from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-
-
-from .models import Question
+from .models import Question, Choice
 
 # Create your views
 
-# index
+# index view
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')
 	context = {'latest_question_list': latest_question_list}
 	return render(request, 'polls/index.html', context)
 
-# detail
+# details view
 def detail(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'polls/detail.html', {'question': question})
 
-# results
-def results(request, question_id):
-	response = "You are viewing the results of question %s."
-	return HttpResponse(response % question_id)
-
-# Vote
+# Vote view
 def vote(request, question_id):
-	return HttpResponse("You are voting on question %s." % question_id)
+    p = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+        # request.POST is a dictionary object that
+        # lets you access submitted values/data by key name
+        # returns id of selected choice as string
+        # Note that Django also provides request.GET
+        # for accessing GET data in the same way
+
+    except (KeyError, Choice.DoesNotExist):
+        # raise KeyError if choice wasn't provided
+        # Redisplay the question voting form with an error message
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message':  "Oops! You didn't select a choice.",
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+# Always return an HttpResponseRedirect after successfully
+# dealing with a POST data. This prevents data from being posted twice
+# if a user hits the back button
+#  HttpResponseRedirect takes a single argument: the URL
+# to which the user will be redirected
+# the reverse function helps to avoid having to hardcode a URL in the view
+    return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+# result views
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
